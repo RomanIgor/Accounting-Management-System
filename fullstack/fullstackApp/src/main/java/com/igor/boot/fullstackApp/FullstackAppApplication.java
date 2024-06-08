ab979862167cb88977a4bb305fe886699f5d4087
roman <romanigor.conect@gmail.com>
2024-06-08 13:39:34 +0200

    changed the database from Msql to postgreSql

--- fullstack/fullstackApp/src/main/java/com/igor/boot/fullstackApp/FullstackAppApplication.java
+++ fullstack/fullstackApp/src/main/java/com/igor/boot/fullstackApp/FullstackAppApplication.java
@@ -1,14 +1,11 @@
 package com.igor.boot.fullstackApp;
 
-import com.vaadin.flow.server.PWA;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@PWA(
-		name = "Accounting",
-		shortName = "ACC"
-)
+
 public class FullstackAppApplication {
 
 	public static void main(String[] args) {
